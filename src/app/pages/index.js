668dc87dea74b0893e32587a6d2220910789ae26
denisjosemendozavidal <denismendozavidal@gmail.com>
2024-10-05 08:15:53 -0400

    Starting The Project

---
+++ src/app/pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="w-screen overflow-x-hidden">
+      {/*Navbar goes here*/}
+      <div className="w-full flex flex-col gap-12 bg-customYellow1 items-center pt-2">
+        {/*Other sections goes here*/}
+      </div>
+      {/*Footer goes here*/}
+    </div>
+  );
+}
