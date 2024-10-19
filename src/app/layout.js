0f533539482db2ac54be30d69f4cb40c6cf401fb
denisjosemendozavidal <denismendozavidal@gmail.com>
2024-10-19 09:23:16 -0400

    Structuring Done - Next: Branding

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <div className="pagewrapper">
+          <Navbar />
           <main>{children}</main>
           <Footer />
           <WhatsappWidget />
